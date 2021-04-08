cd1026371aac08d8c2c61f01d7424f21cbfa8e51
DebrahWyatt <debrahrwyatt@gmail.com>
2021-04-07 17:44:18 -0700

    js tutorial

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Outside</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta property="og:type" content="website">
+    <meta name="description" content="NeuroTech UVic">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+</head>
+
+<body>
+
+</body>
+
+</html>
