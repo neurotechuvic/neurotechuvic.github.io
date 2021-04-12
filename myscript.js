b319abc21e0f96a16963e5566c5d2624898421f8
danigallegdup <32423076+danigallegdup@users.noreply.github.com>
2021-04-11 17:50:02 -0700

    fixed join us page

--- myscript.js
+++ myscript.js
@@ -1,16 +1,28 @@
 
+// Slideshow
+var slideIndex = 1;
+showDivs(slideIndex);
 
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
 
-console.log("watermellon")
-var mynumber = document.getElementById("myjs").innerHTML;
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
 
-
-var obj = document.getElementById("myjs")
-
-obj.style.color = "blue";
-obj.style.fontSize = "x-large";
-
-console.log(mynumber*10)
-for (var i = 10; i >= 0; i--) {
-	
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demodots");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
 }
