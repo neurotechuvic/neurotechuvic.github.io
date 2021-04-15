8b652abb8092fcd2d043908cd141934c5a27d1b5
DebrahWyatt <debrahrwyatt@gmail.com>
2021-04-14 18:36:06 -0700

    added jquery to all content html pages, header.js now works on all pages

--- header.js
+++ header.js
@@ -1,43 +1,48 @@
 
+var prefix = "./"
+if(!window.location.href.includes("index.html")){
+        prefix = "../"
+}
+
 $(document).ready(function() {
         $('#navbar').append(
                 $('<div>').prop({
                         id: 'navcol-1',
-                        innerHTML: '<a href="../index.html"><img src="imgs/logo_shadow.png" style="width: 91px ;margin: 8px;"></a>'
+                        innerHTML: '<a href="' + prefix + 'index.html"><img src="' + prefix + 'imgs/logo_shadow.png" style="width: 91px ;margin: 8px;"></a>'
                 })
         );
         $('#navbar').append(
                 $('<a>').prop({
                         id: 'navcol-1',
-                        href: "index.html",
+                        href: prefix +"index.html",
                         innerHTML: 'Home'
                 })
         );
         $('#navbar').append(
                 $('<a>').prop({
                         id: 'navcol-1',
-                        href: "team.html",
+                        href: prefix + "content/team.html",
                         innerHTML: 'Team'
                 })
         );
         $('#navbar').append(
                 $('<a>').prop({
                         id: 'navcol-1',
-                        href: "resources.html",
+                        href: prefix + "content/resources.html",
                         innerHTML: 'Resource'
                 })
         );
                         $('#navbar').append(
                 $('<a>').prop({
                         id: 'navcol-1',
-                        href: "projects.html",
+                        href: prefix + "content/projects.html",
                         innerHTML: 'Projects'
                 })
         );
         $('#navbar').append(
                 $('<a>').prop({
                         id: 'navcol-1',
-                        href: "joinUs.html",
+                        href: prefix + "content/joinUs.html",
                         innerHTML: 'Join Us'
                 })
         );                        
