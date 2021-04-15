c872d4211762a609e4c716f9938bd55a0b25b10f
DebrahWyatt <debrahrwyatt@gmail.com>
2021-04-14 18:03:07 -0700

    created header.js, called in index.html, builds header when document loads.

---
+++ header.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function() {
+        $('#navbar').append(
+                $('<div>').prop({
+                        id: 'navcol-1',
+                        innerHTML: '<a href="../index.html"><img src="imgs/logo_shadow.png" style="width: 91px ;margin: 8px;"></a>'
+                })
+        );
+        $('#navbar').append(
+                $('<a>').prop({
+                        id: 'navcol-1',
+                        href: "index.html",
+                        innerHTML: 'Home'
+                })
+        );
+        $('#navbar').append(
+                $('<a>').prop({
+                        id: 'navcol-1',
+                        href: "team.html",
+                        innerHTML: 'Team'
+                })
+        );
+        $('#navbar').append(
+                $('<a>').prop({
+                        id: 'navcol-1',
+                        href: "resources.html",
+                        innerHTML: 'Resource'
+                })
+        );
+                        $('#navbar').append(
+                $('<a>').prop({
+                        id: 'navcol-1',
+                        href: "projects.html",
+                        innerHTML: 'Projects'
+                })
+        );
+        $('#navbar').append(
+                $('<a>').prop({
+                        id: 'navcol-1',
+                        href: "joinUs.html",
+                        innerHTML: 'Join Us'
+                })
+        );                        
+});
+
+
+        // <a href="index.html">Home</a>
+        // <a href="content/team.html">Team</a>
+        // <a href="content/resources.html">Resource</a>
+        // <a href="content/projects.html">Projects</a>
+        // <a href="content/joinUs.html">Join Us</a> 
+
